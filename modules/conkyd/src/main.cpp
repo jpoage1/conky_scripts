03872638cbbbc5cd6609402daaa83d05ab7e5090
Jason <jason@localhost.com>
2025-08-25 22:45:08 -0500

    modified:   src/main.cpp 	modified:   src/main.h

--- modules/conkyd/src/main.cpp
+++ modules/conkyd/src/main.cpp
@@ -8,27 +8,6 @@
 #include "swapinfo.h"
 #include "uptime.h"
 
-struct SystemMetrics {
-  struct CoreStats {
-    size_t core_id;
-    long unsigned idle_time;
-    long unsigned total_time;
-  };
-
-  std::vector<CoreStats> cores;
-  double cpu_frequency_ghz;
-  long mem_used_kb;
-  long mem_total_kb;
-  int mem_percent;
-  long swap_used_kb;
-  long swap_total_kb;
-  int swap_percent;
-  std::string uptime;
-};
-SystemMetrics read_data(std::ifstream &, std::ifstream &, std::ifstream &,
-                        std::ifstream &);
-void print_data(SystemMetrics);
-
 int main() {
   ssh_connection();
 
--- modules/conkyd/src/main.h
+++ modules/conkyd/src/main.h
@@ -32,29 +32,6 @@ class DataStreamProvider {
   virtual std::istream &get_stat_stream() = 0;
 };
 
-// struct ProcDataStreams : public DataStreamProvider {
-//   std::stringstream cpuinfo;
-//   std::stringstream meminfo;
-//   std::stringstream uptime;
-//   std::stringstream stat;
-
-//   std::istream &get_cpuinfo_stream() override { return cpuinfo; }
-//   std::istream &get_meminfo_stream() override { return meminfo; }
-//   std::istream &get_uptime_stream() override { return uptime; }
-//   std::istream &get_stat_stream() override { return stat; }
-// };
-// struct LocalDataStreams : public DataStreamProvider {
-//   std::ifstream cpuinfo;
-//   std::ifstream meminfo;
-//   std::ifstream uptime;
-//   std::ifstream stat;
-
-//   std::istream &get_cpuinfo_stream() override { return cpuinfo; }
-//   std::istream &get_meminfo_stream() override { return meminfo; }
-//   std::istream &get_uptime_stream() override { return uptime; }
-//   std::istream &get_stat_stream() override { return stat; }
-// };
-
 struct LocalDataStreams {
   std::ifstream cpuinfo;
   std::ifstream meminfo;
@@ -62,10 +39,26 @@ struct LocalDataStreams {
   std::ifstream stat;
 };
 
-void print_data(SystemMetrics);
+struct SystemMetrics {
+  struct CoreStats {
+    size_t core_id;
+    long unsigned idle_time;
+    long unsigned total_time;
+  };
 
-SystemMetrics read_data(LocalDataStreams &);
-// ProcDataStreams get_ssh_streams();
-LocalDataStreams get_local_file_streams();
+  std::vector<CoreStats> cores;
+  double cpu_frequency_ghz;
+  long mem_used_kb;
+  long mem_total_kb;
+  int mem_percent;
+  long swap_used_kb;
+  long swap_total_kb;
+  int swap_percent;
+  std::string uptime;
+};
+
+SystemMetrics read_data(std::ifstream &, std::ifstream &, std::ifstream &,
+                        std::ifstream &);
+void print_data(SystemMetrics);
 
 void print_metrics(SystemMetrics);
