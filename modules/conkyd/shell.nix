1ae0768c5493e775032d566fa95248996e8ec09e
Jason <jason@localhost.com>
2025-08-26 00:24:10 -0500

    new file:   shell.nix

---
+++ modules/conkyd/shell.nix
@@ -0,0 +1,26 @@
+{ pkgs ? import <nixpkgs> { } }:
+
+pkgs.mkShell {
+  name = "conkyd-dev-env";
+
+  # C++ Toolchain and Build System
+  buildInputs = with pkgs; [
+    # Compiler
+    gcc
+
+    # Build system
+    cmake
+    libssh
+    libssh2
+    # openssl
+  ];
+
+
+  # Shell-specific settings
+  shellHook = ''
+    echo "Entering a Nix development shell for conkyd..."
+    echo "C++ compiler available: $(which g++)"
+    echo "Build system available: $(which cmake)"
+    echo "libssh headers and library are now available for your project."
+  '';
+}
